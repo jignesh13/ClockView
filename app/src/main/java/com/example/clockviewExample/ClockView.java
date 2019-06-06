6c9cf43d67ebe0d9e59090cda087e369a86c9e00
macmini1 <jigneshkhunt13@gmail.com>
2019-06-06 18:57:06 +0530

    update clock widget

--- app/src/main/java/com/example/clockviewExample/ClockView.java
+++ app/src/main/java/com/example/clockviewExample/ClockView.java
@@ -1,16 +1,27 @@
 package com.example.clockviewExample;
 
 import android.content.Context;
+import android.content.res.TypedArray;
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.BitmapShader;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.LinearGradient;
 import android.graphics.Paint;
 import android.graphics.Path;
+import android.graphics.PorterDuff;
+import android.graphics.PorterDuffXfermode;
 import android.graphics.Rect;
+import android.graphics.RectF;
 import android.graphics.Shader;
 import android.graphics.Typeface;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
+import android.os.Build;
 import android.os.Handler;
+import android.support.annotation.DrawableRes;
+import android.support.v4.content.ContextCompat;
 import android.text.Layout;
 import android.text.StaticLayout;
 import android.text.TextPaint;
@@ -22,73 +33,202 @@ import java.time.LocalDateTime;
 import java.time.temporal.ChronoField;
 
 public class ClockView extends View{
+    private  Drawable drawable;
     private Paint paint=new Paint(Paint.ANTI_ALIAS_FLAG);
-    private Bitmap backgroundbitmap;
-    float centerx,centery,radius;
-   private Path minpath,hourpath,secpath;
+    private int resouceid;
+    private Bitmap backgroundbitmap,backimagebitmap;
+    private Paint mpaint;
+    private int hourcolor,mincolor,seccolor,textcolor,dotcolor,backcolor;
+    private float centerx,centery,radius;
+    private Path minpath,hourpath,secpath;
+   private boolean isremoveborder;
     private Handler handler = new Handler();
 
     public ClockView(Context context) {
-        super(context);
+        this(context, null);
     }
 
-    public ClockView(Context context,  AttributeSet attrs) {
-        super(context, attrs);
+    public ClockView(Context context, AttributeSet attributeSet) {
+        this(context, attributeSet, 0);
     }
 
-    public ClockView(Context context,  AttributeSet attrs, int defStyleAttr) {
-        super(context, attrs, defStyleAttr);
+    public void setborder(boolean isremoveborder) {
+        this.isremoveborder = isremoveborder;
+        init();
     }
 
-    public ClockView(Context context,  AttributeSet attrs, int defStyleAttr, int defStyleRes) {
-        super(context, attrs, defStyleAttr, defStyleRes);
+    public boolean isborder() {
+        return isremoveborder;
+    }
+
+    public void setbackImageResource(@DrawableRes int resId){
+         drawable = ContextCompat.getDrawable(getContext(),resId);
+         init();
+
+    }
+
+    public void setTextcolor(int textcolor) {
+        this.textcolor = textcolor;
+        this.dotcolor=textcolor;
+        init();
+    }
+
+    public void setHourcolor(int hourcolor) {
+        this.hourcolor = hourcolor;
+
+    }
+
+    public void setMincolor(int mincolor) {
+        this.mincolor = mincolor;
+
+    }
+
+    public void setSeccolor(int seccolor) {
+        this.seccolor = seccolor;
+
+    }
+
+
+    public ClockView(Context context, AttributeSet attributeSet, int i) {
+        super(context, attributeSet, i);
+        this.paint = new Paint(Paint.ANTI_ALIAS_FLAG);
+
+        if(attributeSet == null){
+            return;
+        }
+
+        TypedArray ta = getContext().obtainStyledAttributes(attributeSet, R.styleable.MyCustomView);
+        backcolor=ta.getColor(R.styleable.MyCustomView_backcolor,Color.WHITE);
+        hourcolor=ta.getColor(R.styleable.MyCustomView_hourcolor,Color.BLACK);
+        mincolor=ta.getColor(R.styleable.MyCustomView_mincolor,Color.BLACK);
+        seccolor=ta.getColor(R.styleable.MyCustomView_seccolor,Color.RED);
+        textcolor=ta.getColor(R.styleable.MyCustomView_txtcolor,Color.BLACK);
+        dotcolor=ta.getColor(R.styleable.MyCustomView_dotcolor,Color.BLACK);
+        drawable = ta.getDrawable(R.styleable.MyCustomView_backimage);
+        isremoveborder=ta.getBoolean(R.styleable.MyCustomView_removeborder,false);
+
+
+
+    }
+    public static Bitmap drawableToBitmap (Drawable drawable) {
+        Bitmap bitmap = null;
+
+        if (drawable instanceof BitmapDrawable) {
+            BitmapDrawable bitmapDrawable = (BitmapDrawable) drawable;
+            if(bitmapDrawable.getBitmap() != null) {
+                return bitmapDrawable.getBitmap();
+            }
+        }
+
+        if(drawable.getIntrinsicWidth() <= 0 || drawable.getIntrinsicHeight() <= 0) {
+            bitmap = Bitmap.createBitmap(1, 1, Bitmap.Config.ARGB_8888); // Single color bitmap will be created of 1x1 pixel
+        } else {
+            bitmap = Bitmap.createBitmap(drawable.getIntrinsicWidth(), drawable.getIntrinsicHeight(), Bitmap.Config.ARGB_8888);
+        }
+
+        Canvas canvas = new Canvas(bitmap);
+        drawable.setBounds(0, 0, canvas.getWidth(), canvas.getHeight());
+        drawable.draw(canvas);
+        return bitmap;
     }
 
     @Override
     protected void onSizeChanged(int w, int h, int oldw, int oldh) {
         super.onSizeChanged(w, h, oldw, oldh);
+
         if (getWidth()>0||getHeight()>0){
-            centerx=getWidth()/2.0f;
-            centery=getHeight()/2.0f;
-            radius=Math.min(getWidth()/2.0f,getHeight()/2.0f);
-            drawBackGroundBitmap();
-            minpath=new Path();
-            float  bottomY = getWidth()/1.7f ;
-            minpath.moveTo(getWidth()/2.0f,(radius/14)*3.5f);
-            minpath.lineTo(centerx-4,(radius/14)*3.5f);
-            minpath.lineTo(centerx-8,bottomY);
-            minpath.lineTo(centerx+8,bottomY);
-            minpath.lineTo(centerx+4,(radius/14)*3.5f);
 
-            hourpath=new Path();
-            hourpath.moveTo(getWidth()/2.0f,(radius/14)*7.0f);
-            hourpath.lineTo(centerx-5,(radius/14)*7.0f);
-            hourpath.lineTo(centerx-12,bottomY);
-            hourpath.lineTo(centerx+12,bottomY);
-            hourpath.lineTo(centerx+5,(radius/14)*7.0f);
-
-            secpath=new Path();
-            secpath.moveTo(getWidth()/2.0f,(radius/14)*5.0f);
-            secpath.lineTo(centerx-2,(radius/14)*5.0f);
-            secpath.lineTo(centerx-5,bottomY);
-            secpath.lineTo(centerx+5,bottomY);
-            secpath.lineTo(centerx+2,(radius/14)*5.0f);
-
-            paint.setStyle(Paint.Style.FILL);
-
+            init();
         }
+    }
+    private void init(){
+        centerx=getWidth()/2.0f;
+        centery=getHeight()/2.0f;
+        radius=Math.min(getWidth()/2.0f,getHeight()/2.0f);
+        drawBackGroundBitmap();
+        if (drawable!=null){
+            backimagebitmap=transform(drawableToBitmap(drawable));
+        }
+
+        mpaint = new Paint(Paint.ANTI_ALIAS_FLAG);
+        mpaint.setStyle(Paint.Style.STROKE);
+        mpaint.setStrokeWidth(radius/14.0f);
+        mpaint.setColor(Color.WHITE);
+        setLayerType(LAYER_TYPE_SOFTWARE, mpaint);
+        mpaint.setShadowLayer(radius/21.0f, 0, 0, Color.rgb(70,70,70));;
+
+
+
+        minpath=new Path();
+        float  bottomY = getWidth()/2.0f ;
+        float toppadding=(radius/14.0f)*4.0f;
+        float width=radius/49.0f;
+        minpath.moveTo(getWidth()/2.0f,toppadding);
+        minpath.addRoundRect(new RectF(centerx-width,toppadding,centerx+width,bottomY),width,width, Path.Direction.CW);
+
+
+        toppadding=(radius/14.0f)*8.0f;
+        hourpath=new Path();
+        hourpath.moveTo(getWidth()/2.0f,toppadding);
+        hourpath.addRoundRect(new RectF(centerx-width,toppadding,centerx+width,bottomY),width,width, Path.Direction.CW);
+
+
+
+        bottomY = getWidth()/1.7f ;
+        width=radius/91.7f;
+        toppadding=radius/5.0f;
+        secpath=new Path();
+        secpath.moveTo(getWidth()/2.0f,toppadding);
+        secpath.addRoundRect(new RectF(centerx-width,toppadding,centerx+width,bottomY),width,width, Path.Direction.CW);
+
+
+        paint.setStyle(Paint.Style.FILL);
+    }
+    public Bitmap transform(Bitmap source) {
+        source=Bitmap.createScaledBitmap(source,getWidth(),getHeight(),false);
+        int size = Math.min(source.getWidth(),source.getHeight());
+        int x = (source.getWidth() - size) / 2;
+        int y = (source.getHeight() - size) / 2;
+
+        Bitmap squaredBitmap = Bitmap.createBitmap(source, x, y, size, size);
+        if (squaredBitmap != source) {
+            source.recycle();
+        }
+
+        Bitmap bitmap = Bitmap.createBitmap(size, size, source.getConfig());
+
+        Canvas canvas = new Canvas(bitmap);
+        Paint paint = new Paint();
+        BitmapShader shader = new BitmapShader(squaredBitmap,
+                BitmapShader.TileMode.CLAMP, BitmapShader.TileMode.CLAMP);
+        paint.setShader(shader);
+        paint.setAntiAlias(true);
+
+        float r = size / 2f;
+        canvas.drawCircle(r, r, r-(r/14.0f), paint);
+
+        squaredBitmap.recycle();
+        return bitmap;
     }
 
     @Override
     protected void onDraw(Canvas canvas) {
-        super.onDraw(canvas);
-      if (backgroundbitmap!=null)canvas.drawBitmap(backgroundbitmap,0,0,paint);
+        Path path=new Path();
+        path.addCircle(centerx,centery,radius, Path.Direction.CW);
+        canvas.clipPath(path);
+        paint.setStyle(Paint.Style.FILL);
+        paint.setColor(backcolor);
+        if (backimagebitmap!=null)canvas.drawBitmap(backimagebitmap,0,0,paint);
+        else {
+            if (isremoveborder)canvas.drawCircle(centerx,centery,radius-radius/14.0f,paint);
+            else canvas.drawCircle(centerx,centery,radius,paint);
+        }
 
+        if (!isremoveborder)canvas.drawCircle(centerx,centery,radius-radius/28.0f,mpaint);
 
-
-
-        paint.setColor(Color.argb(255,255,255,255));
-        canvas.drawCircle(centerx,centery,20,paint);
+        if (backgroundbitmap!=null)canvas.drawBitmap(backgroundbitmap,0,0,paint);
+        paint.setColor(hourcolor);
+        canvas.drawCircle(centerx,centery,radius/25.7f,paint);
         LocalDateTime now = LocalDateTime.now();
 
 
@@ -97,42 +237,46 @@ public class ClockView extends View{
         int second = now.getSecond();
         int millis = now.get(ChronoField.MILLI_OF_SECOND); // Note: no direct getter available.
         hour=hour>12?hour-12:hour;
-        paint.setColor(Color.argb(245,255,255,255));
+
+        paint.setColor(hourcolor);
+       // canvas.drawCircle(centerx,centerx,radius/98.5f,paint);
+
         canvas.save();
-        canvas.rotate((6.0f*second)+((millis/1000.0f)*6.0f),centerx,centery);
-        canvas.drawPath(secpath,paint);
+        canvas.rotate((hour*30.0f)+((minute/60.0f)*30.0f),centerx,centery);
+        canvas.drawPath(hourpath,paint);
         canvas.restore();
+
+
+
+        paint.setColor(mincolor);
 
         canvas.save();
         canvas.rotate((6.0f*minute)+((second/60.0f)*6.0f),centerx,centery);
         canvas.drawPath(minpath,paint);
         canvas.restore();
 
+        paint.setColor(seccolor);
         canvas.save();
-        canvas.rotate((hour*30.0f)+((minute/60.0f)*30.0f),centerx,centery);
-        canvas.drawPath(hourpath,paint);
+        canvas.rotate((6.0f*second)+((millis/1000.0f)*6.0f),centerx,centery);
+        canvas.drawPath(secpath,paint);
         canvas.restore();
-        paint.setColor(Color.rgb(35,35,35));
-        canvas.drawCircle(centerx,centerx,4,paint);
 
+        canvas.drawCircle(centerx,centerx,radius/45.0f,paint);
+
+
+        super.onDraw(canvas);
     }
 
     private void drawBackGroundBitmap(){
         backgroundbitmap=Bitmap.createBitmap(getWidth(),getHeight(), Bitmap.Config.ARGB_4444);
         Canvas canvas=new Canvas(backgroundbitmap);
         paint.setStyle(Paint.Style.STROKE);
-        float outerwidth=radius/14;
+        float outerwidth=radius/14.0f;
         paint.setStrokeWidth(outerwidth);
-        Shader shader = new LinearGradient(0, 0, getWidth(), getHeight(), Color.rgb(90,90,90), Color.rgb(0,0,0), Shader.TileMode.CLAMP);
+        Shader shader = new LinearGradient(0, 0,0, getHeight(), Color.rgb(215,231,236), Color.rgb(117,117,117), Shader.TileMode.CLAMP);
         paint.setShader(shader);
-        canvas.drawCircle(centerx,centery,radius-outerwidth/2.0f,paint);
-        Shader shader1 = new LinearGradient(0, 0, getWidth(), getHeight(), Color.rgb(0,0,0), Color.rgb(90,90,90), Shader.TileMode.CLAMP);
-        paint.setShader(shader1);
-        canvas.drawCircle(centerx,centerx,radius-outerwidth-outerwidth/2.0f,paint);
+        if (!isremoveborder)canvas.drawCircle(centerx,centery,radius-outerwidth/2.0f,paint);
         paint.setShader(null);
-        paint.setColor(Color.rgb(35,35 ,35));
-        paint.setStyle(Paint.Style.FILL);
-        canvas.drawCircle(centerx,centerx,radius-(2*outerwidth)+2.0f,paint);
         paint.setStyle(Paint.Style.FILL);
         paint.setColor(Color.WHITE);
         int o=1;
@@ -141,11 +285,11 @@ public class ClockView extends View{
         textPaint.setTextSize(radius/8.f);
 
         textPaint.setTextAlign(Paint.Align.RIGHT);
-        textPaint.setColor(Color.WHITE);
-        float minus=2*outerwidth+15;
+        textPaint.setColor(Color.BLACK);
+        float minus=2*outerwidth+5;
         for (int i=-60;i<300;i+=6){
-            float xstart = (float) ((radius-2*outerwidth-10) * Math.cos(Math.toRadians(i)) + centerx);
-            float ystart = (float) ((radius-2*outerwidth-10) * Math.sin(Math.toRadians(i)) + centery);
+            float xstart = (float) ((radius-2*outerwidth) * Math.cos(Math.toRadians(i)) + centerx);
+            float ystart = (float) ((radius-2*outerwidth) * Math.sin(Math.toRadians(i)) + centery);
             float xstart1=0;
             float ystart1=0;
             float w=textPaint.measureText(o+"");
@@ -214,14 +358,17 @@ public class ClockView extends View{
                 ystart1 = (float) ((radius-minus-textPaint.getTextSize()) * Math.sin(Math.toRadians(i)) +centery);
             }
 
-            paint.setColor(Color.WHITE);
+            paint.setColor(Color.BLACK);
             if (i%30==0){
-                canvas.drawCircle(xstart,ystart,5,paint);
+                paint.setColor(dotcolor);
+                canvas.drawCircle(xstart,ystart,radius/78.8f,paint);
+                textPaint.setColor(textcolor);
                 canvas.drawText(o+"",xstart1,ystart1,textPaint);
                 o++;
             }
             else {
-                canvas.drawCircle(xstart,ystart,2.5f,paint);
+                paint.setColor(dotcolor);
+                canvas.drawCircle(xstart,ystart,radius/157.6f,paint);
             }
 
         }
--- app/src/main/java/com/example/clockviewExample/MainActivity.java
+++ app/src/main/java/com/example/clockviewExample/MainActivity.java
@@ -1,24 +1,47 @@
 package com.example.clockviewExample;
 
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.ImageView;
+
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
+    private int resource[]={R.mipmap.pexelsmip,R.mipmap.theme,R.mipmap.theme2,R.mipmap.theme3,R.mipmap.theme5,R.mipmap.theme6};
+    private ClockView clockView;
+    Random random=new Random();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        new java.util.Timer().schedule(
-                new java.util.TimerTask() {
-                    @Override
-                    public void run() {
-                        // your code here
-                        Log.e("Call","calltiem");
-                    }
-                },
-                0,1000
-        );
+        clockView=findViewById(R.id.clockView);
+
+
+
+    }
+
+    public void setImageClick(View view) {
+        clockView.setbackImageResource(resource[random.nextInt(resource.length)]);
+    }
+
+    public void removeBorder(View view) {
+        clockView.setborder(!clockView.isborder());
+    }
+
+    public void hourcolorchange(View view) {
+        clockView.setHourcolor(Color.rgb(random.nextInt(256),random.nextInt(256),random.nextInt(256)));
+    }
+    public void mincolorchange(View view) {
+        clockView.setMincolor(Color.rgb(random.nextInt(256),random.nextInt(256),random.nextInt(256)));
+    }
+    public void seccolorchange(View view) {
+        clockView.setSeccolor(Color.rgb(random.nextInt(256),random.nextInt(256),random.nextInt(256)));
+    }
+    public void textcolorchange(View view) {
+        clockView.setTextcolor(Color.rgb(random.nextInt(256),random.nextInt(256),random.nextInt(256)));
     }
 }
